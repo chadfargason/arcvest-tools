e8b4438f7232dc56550dfee58e3ebc3939282336
chadfargason <chad.fargason@gmail.com>
2025-10-30 13:54:12 -0500

    Enable embedding: Allow site to be embedded on arcvest.com via CSP headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          // Allow this app to be embedded on arcvest.com
+          { 
+            key: 'Content-Security-Policy', 
+            value: "frame-ancestors 'self' https://arcvest.com https://www.arcvest.com" 
+          },
+          // Do NOT set X-Frame-Options - CSP frame-ancestors takes precedence
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
